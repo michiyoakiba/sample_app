e100c7a503541b156a858c4eacb48b38a6215ad0
EC2 Default User <ec2-user@ip-172-31-5-65.ap-northeast-1.compute.internal>
2022-12-13 09:29:00 +0000

    ７章

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,43 @@
+class ListsController < ApplicationController
+  def new
+    # Viewへ渡すためのインスタンス変数に空のmodelオブジェクトを生成する。
+    @list = List.new
+  end
+
+  def create
+    list = List.new(list_params)
+    list. save
+    # redirect_to '/top' を削除して、以下のコードに変更
+    redirect_to list_path(list.id)
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id]) # データ（レコード）を1件取得
+    list.destroy #データ（レコード）を削除
+    redierct_to '/lists' # 投稿一覧画面へリダイレクト
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+end
